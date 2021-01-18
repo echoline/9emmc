3c5a94da0c02b782b44caa68acb81564fccc299a
glenda <glenda@9front.local>
2021-01-18 18:30:55 +0000

    less crappy, still crappy audio for compute module 3+

--- bcm/audiopwm.c
+++ bcm/audiopwm.c
@@ -16,6 +16,8 @@
 static u32int *pwmregs = (u32int*)PWMREGS;
 static u32int *clkregs = (u32int*)CLKREGS;
 
+double volume[2] = { 1.0, 1.0 };
+
 enum {
 	ClkCtl = 40,
 	ClkDiv = 41,
@@ -43,8 +45,6 @@ typedef struct Ctlr Ctlr;
 
 struct Ctlr
 {
-	int active;
-
 	Audio *adev;
 };
 
@@ -52,25 +52,25 @@ static long
 audiowrite(Audio*, void *vp, long n, vlong)
 {
 	uchar *p, *e;
-	u16int sample;
+	s32int sample;
+	uchar i;
 
 	p = vp;
-	e = (uchar*)vp + n;
+	e = p + n;
 
 	while (p < e) {
-		sample = *(u16int*)p;
-		sample >>= 3;
-		sample *= 0.75;
-		p += 2;
-		*(u32int*)(pwmregs + PwmFifo) = sample;
+		for (i = 0; i < 2; i++, p += 2) {
+			sample = *((char*)&p[1]);
+			sample *= 0x100;
+			sample |= p[0];
+			sample *= volume[i];
+			sample += 0x8000;
+			sample &= 0xFFFF;
+			sample >>= 4;
+			*(pwmregs + PwmFifo) = (u16int)sample;
+		}
 
-		sample = *(u16int*)p;
-		sample >>= 3;
-		sample *= 0.75;
-		p += 2;
-		*(u32int*)(pwmregs + PwmFifo) = sample;
-
-		while((*(u32int*)(pwmregs + PwmSta)) & Full);
+		while((*(pwmregs + PwmSta)) & Full);
 	}
 
 	return p - (uchar*)vp;
@@ -82,19 +82,38 @@ audioclose(Audio *, int)
 }
 
 static Volume voltab[] = {
+	[0] "master", 0, 100, Stereo, 0,
 	0,
 };
+
+static int
+getvol(Audio*, int, int a[2])
+{
+	a[0] = volume[0] * 100.0;
+	a[1] = volume[1] * 100.0;
+
+	return 0;
+}
 
 static long
 audiovolread(Audio *adev, void *a, long n, vlong)
 {
-	return genaudiovolread(adev, a, n, 0, voltab, 0, 0);
+	return genaudiovolread(adev, a, n, 0, voltab, getvol, 0);
+}
+
+static int
+setvol(Audio*, int, int a[2])
+{
+	volume[0] = a[0] / 100.0;
+	volume[1] = a[1] / 100.0;
+
+	return 0;
 }
 
 static long
 audiovolwrite(Audio *adev, void *a, long n, vlong)
 {
-	return genaudiovolwrite(adev, a, n, 0, voltab, 0, 0);
+	return genaudiovolwrite(adev, a, n, 0, voltab, setvol, 0);
 }
 
 static long
@@ -133,21 +152,16 @@ reset(Audio *adev, Ctlr *ctlr)
 	gpiosel(RIGHT, Alt0);
 	microdelay(2);
 
-//	*(clkregs+ClkCtl) = Password | (1 << 5);
-
 	*(clkregs+ClkDiv) = Password | 0x2000;
 	*(clkregs+ClkCtl) = Password | ClkEnable | (ClkSrcOsc + ClkPLLCPer);
 	microdelay(2);
-
-//	*(pwmregs+PwmCtl) = 0;
-//	microdelay(2);
 
 	*(pwmregs+PwmRng1) = 0x1624; // 44100 Hz?
 	*(pwmregs+PwmRng2) = 0x1624;
 	*(pwmregs+PwmCtl) = PwEn1 | UseF1 | PwEn2 | UseF2 | ClrFifo;
 	microdelay(2);
 
-	print("#A%d: %s 0x%p clk 0x%p\n", adev->ctlrno, adev->name, pwmregs, clkregs);
+	print("#A%d: %s\n", adev->ctlrno, adev->name);
 
 	adev->ctlr = ctlr;
 	adev->write = audiowrite;
@@ -156,8 +170,6 @@ reset(Audio *adev, Ctlr *ctlr)
 	adev->volwrite = audiovolwrite;
 	adev->status = audiostatus;
 	adev->buffered = audiobuffered;
-
-	ctlr->active = 1;
 
 	return 0;
 }
